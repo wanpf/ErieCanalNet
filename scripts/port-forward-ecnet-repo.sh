88d07b1be6d3d069a6c437161bf447bbfcfd5f42
Cybwan <baili@flomesh.io>
2023-03-28 09:20:11 +0800

    Code optimization

--- scripts/port-forward-ecnet-repo.sh
+++ scripts/port-forward-ecnet-repo.sh
@@ -3,7 +3,7 @@
 # shellcheck disable=SC1091
 set -aueo pipefail
 
-source .env
+K8S_NAMESPACE=ecnet-system
 
 ECNET_POD=$(kubectl get pods -n "$K8S_NAMESPACE" --no-headers  --selector app=ecnet-controller | awk 'NR==1{print $1}')
 
