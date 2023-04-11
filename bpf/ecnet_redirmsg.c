2ee9a9d66cb46d6ed25dbe49832dd0d3c64516eb
cybwan <baili@flomesh.io>
2023-04-11 09:33:29 +0800

    Code optimization

--- bpf/ecnet_redirmsg.c
+++ bpf/ecnet_redirmsg.c
@@ -18,9 +18,9 @@ __section("sk_msg") int ecnet_redirmsg(struct sk_msg_md *msg)
     }
 
 #ifdef DEBUG
-    debugf("ecnet_redirmsg ecnet_sock_pair hah pair.dip:dport = %pI4:%d",
+    debugf("ecnet_redirmsg ecnet_sock_pair dir pair.dip:dport = %pI4:%d",
            &p.dip, bpf_ntohs(p.dport));
-    debugf("ecnet_redirmsg ecnet_sock_pair hah pair.sip:sport = %pI4:%d",
+    debugf("ecnet_redirmsg ecnet_sock_pair dir pair.sip:sport = %pI4:%d",
            &p.sip, bpf_ntohs(p.sport));
     long ret = bpf_msg_redirect_hash(msg, &ecnet_sock_pair, &p, BPF_F_INGRESS);
     if (ret)
