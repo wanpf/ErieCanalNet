965f263ff5e681bfba3dbfb0d52d518ee28f71f6
cybwan <baili@flomesh.io>
2023-04-21 10:31:21 +0800

    Code optimization

--- bpf/ecnet_connect.c
+++ bpf/ecnet_connect.c
@@ -4,56 +4,6 @@
 #include <linux/bpf.h>
 #include <linux/in.h>
 
-static inline int ecnet_udp_con4(struct bpf_sock_addr *ctx)
-{
-    if (bpf_htons(ctx->user_port) != DNS_CAPTURE_PORT) {
-        return 1;
-    }
-
-    __u64 uid = bpf_get_current_uid_gid() & 0xffffffff;
-    if (uid == 0 || uid == PROXY_USER_ID) {
-        return 1;
-    }
-
-    __u32 dst_ip = ctx->user_ip4;
-    if ((dst_ip & 0xff) == 0x7f) {
-        // call local, bypass.
-        return 1;
-    }
-
-    __u64 cookie = bpf_get_socket_cookie_addr(ctx);
-
-#ifdef DEBUG
-    __u64 cgrp_id = bpf_get_current_cgroup_id();
-    debugf("ecnet_udp_con4 [DNS Query]: dst ip: %pI4 port: %d", &dst_ip,
-           bpf_ntohs(ctx->user_port));
-    debugf("ecnet_udp_con4 [DNS Query]: cke: %d cgid: %d uid: %d", cookie,
-           cgrp_id, uid);
-#endif
-
-    struct origin_info origin;
-    memset(&origin, 0, sizeof(origin));
-    origin.ip = ctx->user_ip4;
-    origin.port = ctx->user_port;
-
-    if (bpf_map_update_elem(&ecnet_sess_dest, &cookie, &origin, BPF_ANY)) {
-        printk("ecnet_udp_con4 [DNS Query]: Update origin cookie failed: %d",
-               cookie);
-    }
-
-    __u32 bridge_ip = bpf_htonl(BRIDGE_IP);
-    __u16 bridge_port = bpf_htons(DNS_PROXY_PORT);
-
-#ifdef DEBUG
-    debugf("ecnet_udp_con4 [DNS Query]: bri ip: %pI4 port: %d uid: %d",
-           &bridge_ip, bpf_ntohs(bridge_port), uid);
-#endif
-
-    ctx->user_port = bridge_port;
-    ctx->user_ip4 = bridge_ip;
-    return 1;
-}
-
 static inline int ecnet_tcp_con4(struct bpf_sock_addr *ctx)
 {
     if (bpf_htons(ctx->user_port) == ECNET_PROXY_PORT) {
@@ -111,8 +61,6 @@ __section("cgroup/connect4") int ecnet_sock_connect4(struct bpf_sock_addr *ctx)
     switch (ctx->protocol) {
     case IPPROTO_TCP:
         return ecnet_tcp_con4(ctx);
-    case IPPROTO_UDP:
-        return ecnet_udp_con4(ctx);
     default:
         return 1;
     }
